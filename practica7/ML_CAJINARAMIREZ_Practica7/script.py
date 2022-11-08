eb1a7e59cc448a3849f7044f54d1b4a0973154ba
Physicworld <domingo.cajina@hotmail.com>
2022-11-07 23:07:24 -0600

    Neural Nets

---
+++ practica7/ML_CAJINARAMIREZ_Practica7/script.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+# Loading Models
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+from sklearn.model_selection import train_test_split
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.metrics import classification_report
+
+names = [
+    "KNN",
+    "SVM",
+    "Perceptron"
+]
+
+classifiers = [
+    KNeighborsClassifier(n_neighbors=5),
+    SVC(kernel="rbf", C=10, gamma = 0.1),
+    Perceptron(eta0 = 0.3)
+]
+
+dataset1 = pd.read_csv("dataset_classifiers1.csv").values
+dataset2 = pd.read_csv("dataset_classifiers2.csv").values
+dataset3 = pd.read_csv("dataset_classifiers3.csv").values
+datasets = [(dataset1[:,:2], dataset1[:,2]), (dataset2[:,:2], dataset2[:,2]),(dataset3[:,:2], dataset3[:,2])]
+
+
+figure = plt.figure(figsize=(27, 9))
+i = 1
+# iterate over datasets
+for ds_cnt, ds in enumerate(datasets):
+    # preprocess dataset, split into training and test part
+    X, y = ds
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.4, random_state=42
+    )
+
+    x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+    y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+
+    # just plot the dataset first
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    if ds_cnt == 0:
+        ax.set_title("Input data")
+    # Plot the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors="k")
+    # Plot the testing points
+    ax.scatter(
+        X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolors="k"
+    )
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    i += 1
+
+    # iterate over classifiers
+    for name, clf in zip(names, classifiers):
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        clf.fit(X_train, y_train)
+        labels = clf.predict(X_train)
+        score = clf.score(X_test, y_test)
+        DecisionBoundaryDisplay.from_estimator(
+            clf, X, cmap=cm, alpha=0.8, ax=ax, eps=0.5
+        )
+
+        # Plot the training points
+        ax.scatter(
+            X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors="k"
+        )
+        # Plot the testing points
+        ax.scatter(
+            X_test[:, 0],
+            X_test[:, 1],
+            c=y_test,
+            cmap=cm_bright,
+            edgecolors="k",
+            alpha=0.6,
+        )
+
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(y_min, y_max)
+        ax.set_xticks(())
+        ax.set_yticks(())
+        if ds_cnt == 0:
+            ax.set_title(name)
+        ax.text(
+            x_max - 0.3,
+            y_min + 0.3,
+            ("%.2f" % score).lstrip("0"),
+            size=15,
+            horizontalalignment="right",
+        )
+        table = classification_report(y_train, labels)
+        print(table)
+        i += 1
+
+plt.tight_layout()
+plt.show()
